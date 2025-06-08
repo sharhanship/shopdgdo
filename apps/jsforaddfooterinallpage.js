76441c9a0858430cef1095c94fbba78c6de740a2
Alisharhan <169098785+Alisharhan2005@users.noreply.github.com>
2025-06-08 13:40:44 +0330

    add footer wirless in all page

---
+++ apps/jsforaddfooterinallpage.js
@@ -0,0 +1,11 @@
+// در انتهای بدنه صفحه یا در یک فایل JS جداگانه
+document.addEventListener('DOMContentLoaded', function() {
+  fetch('/pages/nav-down.html')
+    .then(response => response.text())
+    .then(data => {
+      document.body.insertAdjacentHTML('beforeend', data);
+    })
+    .catch(error => {
+      console.error('خطا در بارگذاری فوتر:', error);
+    });
+});
